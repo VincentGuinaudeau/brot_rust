cc01557f2db02355edf772accbdebbd484947e1f
Vincent Guinaudeau <vincent.guinau@gmail.com>
2022-07-11 15:02:05 +0200

    removing dead code

--- src/output/view.rs
+++ src/output/view.rs
@@ -9,33 +9,21 @@ pub struct View {
 	x_size:   i32,
 	y_size:   i32,
 	position: Point,
-	scale:    f32,
 
 	/*
 	 * internal computed properties
 	 */
-	smaller_size: i32,
-	longer_size: i32,
-	// The maxium distance around `position` a point can be to be in the view, squared to avoid doing a sqrt when computing
-	squared_radius: f32,
 	// The distance bewteen two pixels in the complex plan
 	step: f32
 }
 
 impl View {
 	pub fn new (x_size: i32, y_size: i32, position: Point, scale: f32) -> View {
-		let smaller_size   = if x_size < y_size { x_size } else { y_size };
 		let longer_size    = if x_size < y_size { y_size } else { x_size };
-		let ratio          = (smaller_size as f32) / (longer_size as f32);
-		let smaller_scale  = scale * ratio;
 		View {
 			x_size,
 			y_size,
 			position,
-			scale,
-			smaller_size,
-			longer_size,
-			squared_radius: scale * scale + smaller_scale * smaller_scale,
 			step: (scale * 2.) / longer_size as f32,
 		}	
 	}
