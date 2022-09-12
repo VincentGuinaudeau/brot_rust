c7fd61dbbff6e93b2b02e6efdcc8ccee732a5604
Vincent Guinaudeau <vincent.guinau@gmail.com>
2022-09-12 07:24:58 +0200

    Trying to optimize the verification and coordinates computation with simd instructions. Less success.

--- src/point_checker/vectorized.rs
+++ src/point_checker/vectorized.rs
@@ -21,7 +21,7 @@ impl Checker for VectorizedChecker {
 
 	fn push_batch(&mut self, view: &View, args: &Args, mut batch: PinBatch) {
 
-		// state for computing the 
+		// state for computing the points
 
 		let mut drum_r         = f64x8::splat(0.);
 		let mut drum_i         = f64x8::splat(0.);
@@ -45,6 +45,22 @@ impl Checker for VectorizedChecker {
 		let mut trace_iter = batch.traces.iter_mut();
 
 		let mut current_traces: [Option< &mut Trace >; 8] = [ None, None, None, None, None, None, None, None ];
+
+		// state for computing the coordinates
+
+		let mut count = 0;
+
+		let step        = f64x8::splat(view.step());
+		let position_r  = f64x8::splat(view.position().r());
+		let position_i  = f64x8::splat(view.position().i());
+		let half_x_size = i32x8::splat(view.x_size() / 2);
+		let half_y_size = i32x8::splat(view.y_size() / 2);
+		let zero        = i32x8::splat(0);
+		let x_size      = i32x8::splat(view.x_size());
+		let y_size      = i32x8::splat(view.y_size());
+
+		let mut r = f64x8::splat(0.);
+		let mut i = f64x8::splat(0.);
 
 		let mut coords:Vec< (u32, u32) > = vec![];
 
@@ -90,68 +106,50 @@ impl Checker for VectorizedChecker {
 
 			// If a trace is done, unload it, maybe write the coords, and maybe load another one
 			if mask_done.any() {
-				for i in 0..8 {
-					if mask_done.test(i) {
+				for index in 0..8 {
+					if mask_done.test(index) {
+						let trace = current_traces[index].as_mut().unwrap();
+						if 
+							trace.status() == TraceStatus::Outside &&
+							(args.range_start..args.range_stop).contains(&trace.len())
+						{
+							for point in trace.iter() {
+								r[count] = point.r();
+								i[count] = point.i();
+								count += 1;
+
+								if count == 8 {
+
+									let x:i32x8 = ((r - position_r) / step).floor().cast() + half_x_size;
+									let y:i32x8 = ((i - position_i) / step).floor().cast() + half_y_size;
+
+									let accepted = x.lanes_ge(zero) & x.lanes_lt(x_size) & y.lanes_ge(zero) & y.lanes_lt(y_size);
+
+									for i in 0..8 {
+										if accepted.test(i) {
+											coords.push((x[i] as u32, y[i] as u32));
+										}
+									}
+									count = 0;
+								}
+							}
+						}
 
 						let next_trace = trace_iter.next();
 						if let Some(trace) = next_trace {
 							let origin = trace.origin();
-							drum_origin_r[i] = origin.r();
-							drum_origin_i[i] = origin.i();
-							drum_r[i] = 0.;
-							drum_i[i] = 0.;
-							drum_squared_r[i] = 0.;
-							drum_squared_i[i] = 0.;
-							current_traces[i] = Some( trace );
-							loop_count[i] = 0;
+							drum_origin_r[index] = origin.r();
+							drum_origin_i[index] = origin.i();
+							drum_r[index] = 0.;
+							drum_i[index] = 0.;
+							drum_squared_r[index] = 0.;
+							drum_squared_i[index] = 0.;
+							current_traces[index] = Some( trace );
+							loop_count[index] = 0;
 						}
 						else {
-							loaded_lanes.set(i, false);
+							loaded_lanes.set(index, false);
 						}
-					}
-				}
-			}
-		}
-
-		// now we look at all the traces, takes those that are inboud and compute the coordinates
-		let trace_iter = batch.traces.iter();
-		let mut count = 0;
-
-		let step        = f64x8::splat(view.step());
-		let position_r  = f64x8::splat(view.position().r());
-		let position_i  = f64x8::splat(view.position().i());
-		let half_x_size = i32x8::splat(view.x_size() / 2);
-		let half_y_size = i32x8::splat(view.y_size() / 2);
-		let zero        = i32x8::splat(0);
-		let x_size      = i32x8::splat(view.x_size());
-		let y_size      = i32x8::splat(view.y_size());
-
-		let mut r = f64x8::splat(0.);
-		let mut i = f64x8::splat(0.);
-
-		for trace in trace_iter {
-			if 
-				trace.status() == TraceStatus::Outside &&
-				(args.range_start..args.range_stop).contains(&trace.len())
-			{
-				for point in trace.iter() {
-					r[count] = point.r();
-					i[count] = point.i();
-					count += 1;
-
-					if count == 8 {
-
-						let x:i32x8 = ((r - position_r) / step).floor().cast() + half_x_size;
-						let y:i32x8 = ((i - position_i) / step).floor().cast() + half_y_size;
-
-						let accepted = x.lanes_ge(zero) & x.lanes_lt(x_size) & y.lanes_ge(zero) & y.lanes_lt(y_size);
-
-						for i in 0..8 {
-							if accepted.test(i) {
-								coords.push((x[i] as u32, y[i] as u32));
-							}
-						}
-						count = 0;
 					}
 				}
 			}
